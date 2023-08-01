705b9d2d17f5bd83dba70d9d98f8cb14042ccc52
Your Name <you@example.com>
2023-08-01 11:36:40 +0800

    first commit

---
+++ library/build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.kotlinAndroid)
+}
+
+android {
+    namespace = "com.clovero.mmkv"
+    compileSdk = 33
+
+    defaultConfig {
+        minSdk = 16
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.test.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+    api("com.tencent:mmkv:1.3.0")
+    api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+}
---
+++ library/src/main/java/com/clovero/mmkv/MMKVOwner.kt
@@ -0,0 +1,117 @@
+package com.clovero.mmkv
+
+import com.tencent.mmkv.MMKV
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlin.properties.ReadOnlyProperty
+import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty
+
+interface MMKVOwner {
+    val mmkv: MMKV
+}
+
+class DefaultMMKVOwner(override val mmkv: MMKV) : MMKVOwner
+
+fun MMKV.toMMKVOwner(): MMKVOwner {
+    return DefaultMMKVOwner(this)
+}
+
+fun MMKVOwner.boolValue(default: Boolean = false, key: String? = null): MMKVProperty<Boolean> {
+    return MMKVProperty({ mmkv.decodeBool(it, default) }, mmkv::encode, key)
+}
+
+fun MMKVOwner.intValue(default: Int = 0, key: String? = null): MMKVProperty<Int> {
+    return MMKVProperty({ mmkv.decodeInt(it, default) }, mmkv::encode, key)
+}
+
+fun MMKVOwner.longValue(default: Long = 0L, key: String? = null): MMKVProperty<Long> {
+    return MMKVProperty({ mmkv.decodeLong(it, default) }, mmkv::encode, key)
+}
+
+fun MMKVOwner.floatValue(default: Float = 0f, key: String? = null): MMKVProperty<Float> {
+    return MMKVProperty({ mmkv.decodeFloat(it, default) }, mmkv::encode, key)
+}
+
+fun MMKVOwner.doubleValue(default: Double = 0.0, key: String? = null): MMKVProperty<Double> {
+    return MMKVProperty({ mmkv.decodeDouble(it, default) }, mmkv::encode, key)
+}
+
+fun MMKVOwner.bytesValue(key: String? = null): MMKVProperty<ByteArray?> {
+    return MMKVProperty(mmkv::decodeBytes, mmkv::encode, key)
+}
+
+fun MMKVOwner.bytesValue(default: ByteArray, key: String? = null): MMKVProperty<ByteArray> {
+    return MMKVProperty({ mmkv.decodeBytes(it) ?: default }, mmkv::encode, key)
+}
+
+fun MMKVOwner.stringValue(key: String? = null): MMKVProperty<String?> {
+    return MMKVProperty(mmkv::decodeString, mmkv::encode, key)
+}
+
+fun MMKVOwner.stringValue(default: String, key: String? = null): MMKVProperty<String?> {
+    return MMKVProperty({ mmkv.decodeString(it) ?: default }, mmkv::encode, key)
+}
+
+fun MMKVOwner.stringSetValue(key: String? = null): MMKVProperty<Set<String>?> {
+    return MMKVProperty(mmkv::decodeStringSet, mmkv::encode, key)
+}
+
+fun MMKVOwner.stringSetValue(default: String, key: String? = null): MMKVProperty<String?> {
+    return MMKVProperty({ mmkv.decodeString(it) ?: default }, mmkv::encode, key)
+}
+
+private class MMKVStateFlowProperty<T>(
+    private val mmkvProperty: MMKVProperty<T>,
+) : ReadOnlyProperty<MMKVOwner, MutableStateFlow<T>> {
+    private lateinit var _value: MutableStateFlow<T>
+
+    private var lock = this
+
+    override fun getValue(thisRef: MMKVOwner, property: KProperty<*>): MutableStateFlow<T> {
+        if (::_value.isInitialized) {
+            return _value
+        }
+        return synchronized(lock) {
+            if (::_value.isInitialized) {
+                _value
+            } else {
+                val v = createMutableStateFlow(thisRef, property)
+                _value = v
+                v
+            }
+        }
+    }
+
+    private fun createMutableStateFlow(
+        thisRef: MMKVOwner,
+        property: KProperty<*>
+    ): MutableStateFlow<T> {
+        val state = MutableStateFlow(mmkvProperty.getValue(thisRef, property))
+        return object : MutableStateFlow<T> by state {
+            override var value: T
+                get() = mmkvProperty.getValue(thisRef, property)
+                set(value) {
+                    mmkvProperty.setValue(thisRef, property, value)
+                    state.value = value
+                }
+        }
+    }
+}
+
+fun <T> MMKVProperty<T>.asMutableStateFlow(): ReadOnlyProperty<MMKVOwner, MutableStateFlow<T>> {
+    return MMKVStateFlowProperty(this)
+}
+
+class MMKVProperty<V>(
+    private val decode: (String) -> V,
+    private val encode: (String, V) -> Boolean,
+    private var key: String? = null
+) : ReadWriteProperty<MMKVOwner, V> {
+
+    override fun getValue(thisRef: MMKVOwner, property: KProperty<*>): V =
+        decode(key ?: property.name)
+
+    override fun setValue(thisRef: MMKVOwner, property: KProperty<*>, value: V) {
+        encode((key ?: property.name), value)
+    }
+}
