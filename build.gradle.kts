f56729332a0f5ed4ad19cea0a160b0416fa3d5a8
Your Name <you@example.com>
2023-08-01 15:21:48 +0800

    commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,9 @@ plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.kotlinAndroid) apply false
     alias(libs.plugins.androidLibrary) apply false
-    id("com.diffplug.spotless") version "6.20.0"
+    //id("com.diffplug.spotless") version "6.20.0"
 }
 
-allprojects {
-    group = "com.github.clovero"
-}
+//allprojects {
+//    group = "com.github.clovero"
+//}
