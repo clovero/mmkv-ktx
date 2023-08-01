06dabdf43e3302d398ff7d05aa740f46e2e6b38a
Your Name <you@example.com>
2023-08-01 13:51:37 +0800

    commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,6 @@ plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.kotlinAndroid) apply false
     alias(libs.plugins.androidLibrary) apply false
+    id("com.diffplug.spotless") version "6.20.0"
+    id("maven-publish")
 }
